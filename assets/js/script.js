798097033a8b655a8e7da4909a1f83b1a695a881
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-06-28 17:32:29 -0500

    added question and answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,89 +5,106 @@
 // WHEN the game is over, I can save my initials and score
 
 var startButtonEl = document.querySelector("#start-btn");
-var timerEl = document.querySelector("#timer");
+var timerEl = document.querySelector(".timer");
+var scoreEl = document.querySelector(".score");
 var questionsEl = document.querySelector("#question");
-var answersEl = document.querySelector("#answer-btn");
+var optionButtonEl = document.querySelector(".option-btn");
+var options = Array.from(document.getElementsByClassName("option-text"));
 
-let timer = 200
+var timer = 100;
+var score = 0;
+
+var currentQuestion = {};
+var questionCounter = 0;
+var availableQuestions = [];
+
+scoreEl.innerHTML = score;
 
 var questions = [
     {
-        q: "How do you delcare a JavaScript variable?",
-        a: [
-            {text: "variable", correct: false},
-            {text: "declare", correct: false},
-            {text: "var", correct: true},
-            {text: "Hey, JavaScript", correct: false}
-        ]
+        question: "How do you delcare a JavaScript variable?",
+        option1: "variable",
+        option2: "declare",
+        option3: "var",
+        option4: "Hey, JavaScript",
+        answer: 3
     },
     {
-        q: "Which one is NOT an example of an operator?",
-        a: [
-            {text: "+", correct: false},
-            {text: "()", correct: true},
-            {text: "++", correct: false},
-            {text: "--", correct: false}
-        ]
+        question: "Which one is NOT an example of an operator?",
+        option1: "+",
+        option2: "()",
+        option3: "++",
+        option4: "--",
+        answer: 2
     },
     {
-        q: "What is the correct way to set the variable num to 10?",
-        a: [
-            {text: "var number = 10", correct: true},
-            {text: "var number = ten", correct: false},
-            {text: "var number = '10'", correct: false},
-            {text: "All of the above", correct: false}
-        ]
+        question: "What is the correct way to set the variable num to 10?",
+        option1: "var number = 10",
+        option2: "var number = ten",
+        option3: "var number = '10'",
+        option4: "All of the above",
+        answer: 1
     },
     {
-        q: "What is the correct way to write an array?",
-        a: [
-            {text: 'var myArray = "Item1, Item2, Item3"', correct: false},
-            {text: 'var myArray = ["Item1, Item2, Item3"]', correct: false},
-            {text: 'var myArray = ["Item1", "Item2", "Item3"]', correct: true},
-            {text: 'None of the above'}
-        ]
+        question: "What is the correct way to write an array?",
+        option1: 'var myArray = "Item1, Item2, Item3"',
+        option2: 'var myArray = ["Item1, Item2, Item3"]',
+        option3: 'var myArray = ["Item1", "Item2", "Item3"]',
+        option4: 'None of the above',
+        answer: 3
     },
     {
-        q: "What is the correct way to initiate a for loop?",
-        a: [
-            {text: 'for (i = 0, i < myLoop.length, i++)', correct: false},
-            {text: 'for (i = 0; i < myLoop.length; i)', correct: false},
-            {text: 'for (i = 0; i < myLooplength; i++)', correct: false},
-            {text: 'for (i = 0; i < myLoop.length; i++)', correct: true}
-        ]
-    }
+        question: "What is the correct way to write a for loop?",
+        option1: 'for (i = 0, i < myLoop.length, i++)',
+        option2: 'for (i = 0; i < myLoop.length; i)',
+        option3: 'for (i = 0; i < myLooplength; i++)',
+        option4: 'for (i = 0; i < myLoop.length; i++)',
+        answer: 4
+    },
 ];
 
 var startQuiz = function() {
-    console.log("Quiz Started")
-    displayQuestion();
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
     startTimer();
-};
-
-
-var displayQuestion = function() {
-    for (var i = 0; i < questions.length; i++); {
-        console.log(questions);   
-    }
-
+    getQuestion();
 };
 
 var startTimer = function() {
-    setInterval(function() {
+    var timerInterval = setInterval(function() {
         if(timer <= 0 ) {
-            clearInterval(timer=0)
+            clearInterval(timerInterval)
+            //endGame();
         }
         timerEl.innerHTML = timer
         timer -=1
     }, 1000)
-}
+};
 
-var selectAnswer = function() {
-    console.log("You are correct");
+var getQuestion = function() {
+    questionCounter++;
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    questionsEl.innerHTML = currentQuestion.question;
+    options.forEach((option) => {
+        var number = option.dataset["number"];
+        option.innerHTML = currentQuestion["option" + number];
+    });
+    availableQuestions.splice(questionIndex, 1);
+};
 
-}
+var selectAnswer = function(answer) {
+    if (answer === currentQuestion.answer) {
+    score = score + 10;
+    } else {
+        timer -= 10;
+    }
+    setTimeout(getQuestion, 1000);
+};
 
+var endGame = function() {
+
+};
 
 startButtonEl.addEventListener("click", startQuiz);
-answersEl.addEventListener("click", selectAnswer);
